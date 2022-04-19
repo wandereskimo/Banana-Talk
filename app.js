9995a3ad0c07d1a29344e52acec652a3737f4ca3
Baesil <baesilshowcath@gmail.com>
2022-04-19 22:56:23 +0530

    updated script

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var buttonTranslate = document.querySelector("#btn-translate");
+
+console.log(buttonTranslate)
+
+buttonTranslate.addEventListener("click", function eventHandler() {
+    console.log("clicked")
+})
