6f5fb8b25107094c7c7014928a8c85f52909b6f5
Baesil <baesilshowcath@gmail.com>
2022-04-22 03:50:27 +0530

    Feat - added a API

--- app.js
+++ app.js
@@ -2,10 +2,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslationURL(input) {
+    return serverURL + "?" + "text = " + input
+}
+
+function errorHandling(error) {
+	console.log("An error occured", error);
+	alert("Something went Wrong..! Please try after sometime.")
+}
 
 function clickHandler() {
-    outputDiv.innerText = "bsjcbajbdkj  " + txtInput.value; 
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .thenP((response) => response.json())
+        .then((json) => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+          })
+        .catch(errorHandler)
+
+  
 };
 
 btnTranslate.addEventListener("click", clickHandler)
