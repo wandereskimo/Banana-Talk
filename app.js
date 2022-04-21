d20d26d9d483b864ba9eeb872308089c1349db0f
Baesil <baesilshowcath@gmail.com>
2022-04-21 22:47:48 +0530

    Input output processing

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "bsjcbajbdkj  " + txtInput.value; 
 };
 
 btnTranslate.addEventListener("click", clickHandler)
